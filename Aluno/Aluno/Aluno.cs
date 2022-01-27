f8bae6fe19651d4fb580a2cd10020fa596dc1c9a
LucasJesus <lucasdjs2014@hotmail.com>
2022-01-27 04:07:35 -0300

    Exercicios Aluno

---
+++ Aluno/Aluno/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Aluno_
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+
+        public double soma()
+        {
+            return Nota1 + Nota2 + Nota3;
+
+        }
+        public override string ToString()
+        {
+            if (soma() >= 60.00)
+            {
+                return  "NOTA FINAL = " + soma().ToString("F2", CultureInfo.InvariantCulture) +"\n"+
+                "APROVADO"; 
+                
+            }
+            else
+            {
+                double falta = 60.00 - soma();
+                return
+                "NOTA FINAL = " + soma().ToString("F2", CultureInfo.InvariantCulture) +"\n" +
+               "REPROVADO" + "\n"+
+                 "FALTARAM = " + falta.ToString("F2", CultureInfo.InvariantCulture) + " PONTOS";
+
+            }
+        }
+
+    }
+}
---
+++ Aluno/Aluno/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Aluno_;
+
+
+Aluno dados = new Aluno();
+
+Console.Write("Nome do aluno: ");
+dados.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite as três notas do aluno: ");
+dados.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+dados.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+dados.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine(dados);
+
