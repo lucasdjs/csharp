0fef5d90113c5e287a257b98b6902f345be9bbaa
LucasJesus <lucasdjs2014@hotmail.com>
2022-01-27 02:28:56 -0300

    Exercicios
    Signed-off-by: LucasJesus <lucasdjs2014@hotmail.com>

---
+++ Orientação a objeto/3 - Classes, atributos, métodos, membros estáticos/Exercicios/Exercicio 2 Classe/Exercicio 2 Classe/Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercicio_2_Classe;
+using System.Globalization;
+
+SalarioFuncionario p1 = new SalarioFuncionario();
+SalarioFuncionario p2 = new SalarioFuncionario();
+
+Console.WriteLine("Dados do primeiro funcionário: ");
+Console.Write("Nome: ");
+p1.Nome = Console.ReadLine();
+
+Console.Write("Salário: ");
+p1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Dados do segundo funcionário: ");
+Console.Write("Nome: ");
+p2.Nome = Console.ReadLine();
+
+Console.Write("Salário: ");
+p2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double salarioMedio = (p1.salario + p2.salario) / 2;
+Console.WriteLine("Salário Médio: " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+
---
+++ Orientação a objeto/3 - Classes, atributos, métodos, membros estáticos/Problema de estoque/Problema de estoque/Problema de estoque/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Problema_de_estoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEstoque()
+        {
+            double ValorTotal = Preco * Quantidade;
+            return ValorTotal;
+        }
+
+        public void AdicionarProdutos(int quantidade) {
+
+            Quantidade = Quantidade + quantidade;
+
+        }
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public void AlterarValor(double valornovo) { 
+        Preco = valornovo;
+        }
+        public override string ToString()
+        {
+            return Nome + " , $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $" +
+                ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+
+    }
+}
